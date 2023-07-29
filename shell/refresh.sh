a50ca07af7f65239714dee5dc9f2ab87d540fe99
Lewis-C <61985854+Lewis-C@users.noreply.github.com>
2023-07-29 11:32:35 +0100

    Delete shell directory

--- shell/refresh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/bin/python /home/pi/shared/scripts/python/get_weather.py
-export DISPLAY=":0"
-WID=$(xdotool search --onlyvisible --class chromium|head -1)
-xdotool windowactivate ${WID}
-xdotool key ctrl+F5
-
-
